d86f182711aa94fccced2981daeb1bde3f9ff91b
gopikappukuttan <gopika.pa@edstem.com>
2024-11-25 15:29:56 +0530

    Update Calculator.java
    -used switch statement instead of multiple if statements
    -added input validation for numbers
    -added loop for multiple calculations

--- Calculator.java
+++ Calculator.java
@@ -1,65 +1,80 @@
 package com.edstem.ecld.assessment;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Calculator {
-    //method to add 2 variables
+    // Method to add 2 variables
     public static double add(double a, double b) {
         return (a + b);
     }
 
-    //method to substract 2 variables
-    public static double substract(double a, double b) {
+    // Method to subtract 2 variables
+    public static double subtract(double a, double b) {
         return (a - b);
     }
 
-    //method to multiply 2 variables
+    // Method to multiply 2 variables
     public static double multiply(double a, double b) {
         return (a * b);
     }
 
-    //method to divide 2 variables
+    // Method to divide 2 variables
     public static double divide(double a, double b) {
         return (a / b);
     }
 
     public static void main(String[] args) {
-        //making new object of scanner class
         Scanner sc = new Scanner(System.in);
-        //user inputs and method use
-        System.out.print("Enter first number:");
-        double a = sc.nextDouble();
-        System.out.print("Enter second number:");
-        double b = sc.nextDouble();
-        System.out.print("Choose Operation:\n" +
-                "1.Add (+)\n" +
-                "2.Substract(-)\n" +
-                "3.Multiply(*)\n" +
-                "4.Divide(/)\n" +
-                "Enter choice:");
-        int choiceNum = sc.nextInt();
-        //calling method add() if chosen option is 1
-        if (choiceNum == 1) {
-            System.out.println(a + " + " + b + " = " + add(a, b));
-        }
-        //calling method substract() if chosen option is 2
-        if (choiceNum == 2) {
-            System.out.println(a + " - " + b + " = " + substract(a, b));
-        }
-        //calling method multiply() if chosen option is 3
-        if (choiceNum == 3) {
-            System.out.println(a + " * " + b + " = " + multiply(a, b));
-        }
-        //calling method divide() if chosen option is 4
-        if (choiceNum == 4) {
-            //handling division if second integer is 0
-            if(b==0){
-                System.out.println("Undefined (since divison is by zero)");
-            }else{
-                System.out.println(a + " / " + b + " = " + divide(a, b));
+        boolean continueCalculation = true;
+
+        while (continueCalculation) {
+            try {
+                System.out.print("Enter first number: ");
+                double a = sc.nextDouble();
+
+                System.out.print("Enter second number: ");
+                double b = sc.nextDouble();
+
+                System.out.println("Choose Operation:\n" +
+                        "1. Add (+)\n" +
+                        "2. Subtract (-)\n" +
+                        "3. Multiply (*)\n" +
+                        "4. Divide (/)\n" +
+                        "5. Exit");
+                System.out.print("Enter choice: ");
+                int choice = sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println(a + " + " + b + " = " + add(a, b));
+                        break;
+                    case 2:
+                        System.out.println(a + " - " + b + " = " + subtract(a, b));
+                        break;
+                    case 3:
+                        System.out.println(a + " * " + b + " = " + multiply(a, b));
+                        break;
+                    case 4:
+                        if (b == 0) {
+                            System.out.println("Undefined (division by zero is not allowed)");
+                        } else {
+                            System.out.println(a + " / " + b + " = " + divide(a, b));
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Exiting the calculator. Goodbye!");
+                        continueCalculation = false;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please choose a valid option.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                sc.next(); // Clear invalid input
             }
         }
-        //closing scanner
+
         sc.close();
     }
 }
