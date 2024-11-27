2b8ddfa6c372200baaa5384627fa1f3aea75f5c5
gopikappukuttan <gopika.pa@edstem.com>
2024-11-27 14:02:51 +0530

    Update SumOddEven.java

--- Medium/SumOddEven.java
+++ Medium/SumOddEven.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SumOddEven {
 
     public static int warOfNumbers(int[] numbers) {
@@ -13,15 +15,45 @@ public class SumOddEven {
                 sumOdd += number;
             }
         }
-
         // Return the difference between the sum of even and odd numbers
         return Math.abs(sumEven - sumOdd);
     }
 
     public static void main(String[] args) {
-        // Test cases
-        System.out.println(warOfNumbers(new int[]{2, 8, 7, 5}));
-        System.out.println(warOfNumbers(new int[]{12, 90, 75, 55, 9}));
+        Scanner sc = new Scanner(System.in);
+        int n = -1;
+
+        // Input validation for the size of the array
+        while (n <= 0) {
+            System.out.println("Enter the number of elements in the array (positive integer):");
+            if (sc.hasNextInt()) {
+                n = sc.nextInt();
+                if (n <= 0) {
+                    System.out.println("The number of elements must be greater than zero.");
+                }
+            } else {
+                System.out.println("Invalid input! Please enter a valid integer.");
+                sc.next(); // Clear invalid input
+            }
+        }
+
+        int[] numbers = new int[n];
+
+        // Input validation for the array elements
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            while (true) {
+                System.out.print("Element " + (i + 1) + ": ");
+                if (sc.hasNextInt()) {
+                    numbers[i] = sc.nextInt();
+                    break;
+                } else {
+                    System.out.println("Invalid input! Please enter a valid integer.");
+                    sc.next(); // Clear invalid input
+                }
+            }
+        }
+        System.out.println("Result: "+warOfNumbers(numbers));
     }
 }
 
